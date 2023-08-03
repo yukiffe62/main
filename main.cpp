fe53843476135dab108bcf0177fbf2a03de8b9c1
yukiffe62 <140989755+yukiffe62@users.noreply.github.com>
2023-08-03 21:55:54 +0900

    Create main.cpp
    undefined

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void print(){
+  // print algorithm source ,,
+  return;
+}
+
+int main(){
+  cout << "Hello, World!" << endl;
+  print();
+  return 0;
+}
